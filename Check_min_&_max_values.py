24c15707bcdd71bb5abfd853871c4f74ba5ebc82
Saša Pozder <105084546+b3Vk7W2h7G8k0@users.noreply.github.com>
2022-09-16 00:39:19 +0100

    Add files via upload

---
+++ Check_min_&_max_values.py
@@ -0,0 +1,18 @@
+test2={
+    "testname":"Check for min and max",
+    "test":check_for_min_max,
+    "column": "monthid",
+    "table": "DimMonth",
+    "minimum":1,
+    "maximum":12
+}
+
+
+test6={
+    "testname":"Check for min and max",
+    "test":check_for_min_max,
+    "column": "quarter",
+    "table": "DimMonth",
+    "minimum":1,
+    "maximum":4
+}
